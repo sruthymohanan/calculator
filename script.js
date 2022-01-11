21a8bfab40c5f8ccc918db2c4761eded49824bf4
unknown <sruthymohanan532@gmail.com>
2022-01-11 11:12:24 +0530

    calculator app

---
+++ script.js
@@ -0,0 +1,19 @@
+// function displayValue(num){
+//     let res=document.querySelector("#result");
+//     res.value+=num
+// }
+var displayValue=(num)=>result.value+=num
+
+// function clearBox(){
+//     result.value=""
+// }
+
+var clearBox=()=>result.value=""
+
+// function evaluateExpression(){
+//     let expr=result.value;
+//     let out=eval(expr)
+//     result.value=out;
+// }
+
+var evaluateExpression=()=>result.value=eval(result.value)
